fd5a8547e05c43c8912ddf4c56c6b1b78d9dce3a
Ewatervoort <elisemargueritehobbs@gmail.com>
2021-11-01 11:37:15 -0600

    Corrected a mispelled CORS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
+// Dependencies
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
-const cosrs = require("cors")
+const cors = require("cors")
 
+// Routes
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
