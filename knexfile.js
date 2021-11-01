fc62fb4aa0eccc7c6fe8d04f454e492b6c5f6643
Ewatervoort <elisemargueritehobbs@gmail.com>
2021-11-01 11:40:37 -0600

    Changed url back to local database

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 // postgresql://postgres@localhost/postgres
 // postgres://lznraxwi:dqRXPhpXWng9wtSc832oJcWUoozLZLOW@kashin.db.elephantsql.com/lznraxwi
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://lznraxwi:dqRXPhpXWng9wtSc832oJcWUoozLZLOW@kashin.db.elephantsql.com/lznraxwi",
 } = process.env;
 
 module.exports = {
